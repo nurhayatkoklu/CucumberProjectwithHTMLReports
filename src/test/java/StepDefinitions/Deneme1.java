2ea6fe478afa274a5cc1c0a45aa61211d3ced319
Kadir Hacıtemeloğlu <kadir.hacitemeloglu@outlook.com>
2022-09-02 20:22:50 +0300

    oldu mu2 :)

--- src/test/java/StepDefinitions/Deneme1.java
+++ src/test/java/StepDefinitions/Deneme1.java
@@ -4,5 +4,6 @@ public class Deneme1 {
     public static void main(String[] args) {
         System.out.println("Merhaba Github");
         System.out.println("Hi there!");
+        System.out.println("OLdu Mu ya");
     }
 }
