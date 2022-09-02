37104c59819f608d17aa6e01796abdb0bb281b1b
Kadir Hacıtemeloğlu <kadir.hacitemeloglu@outlook.com>
2022-09-02 20:19:58 +0300

    oldu mu :)

--- src/test/java/StepDefinitions/Deneme1.java
+++ src/test/java/StepDefinitions/Deneme1.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class Deneme1 {
     public static void main(String[] args) {
         System.out.println("Merhaba Github");
+        System.out.println("Hi there!");
     }
 }
